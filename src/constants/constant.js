8fbbbe4c39f0f588a994bfcd476507cc0784fc04
Sani Kumar Sharma <82691077+sani9525@users.noreply.github.com>
2023-10-28 08:40:03 +0530

    Add files via upload

---
+++ src/constants/constant.js
@@ -0,0 +1,3 @@
+export const logo='https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.pngegg.com%2Fen%2Fsearch%3Fq%3Drecipes%2BLogo&psig=AOvVaw1nSKaWsBlJXfgQ2qYAcS22&ust=1698469034326000&source=images&cd=vfe&opi=89978449&ved=0CBIQjRxqFwoTCNDxpa24lYIDFQAAAAAdAAAAABAg';
+
+export const API_URL='https://forkify-api.herokuapp.com/api';
